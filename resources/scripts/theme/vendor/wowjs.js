ae14b29b3665b7d22af4177ef6067f156135f287
Mick00 <unconfigured@null.spigotmc.org>
2019-06-27 10:54:10 -0400

    Added Wowjs animation and style for CF7

---
+++ resources/scripts/theme/vendor/wowjs.js
@@ -0,0 +1,14 @@
+import {WOW} from '~/wowjs/dist/wow.js';
+import '~/animate.css/animate.css';
+
+const wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       90,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
