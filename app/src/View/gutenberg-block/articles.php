be04825fd3f4d1a210dd1d86560dfea6b0ab626a
Mick00 <unconfigured@null.spigotmc.org>
2019-07-05 09:29:14 -0400

    Fix responsive issue when display is really small

--- app/src/View/gutenberg-block/articles.php
+++ app/src/View/gutenberg-block/articles.php
@@ -10,8 +10,8 @@ $recent_posts = wp_get_recent_posts( $args, OBJECT );
 $date = strtotime($post->post_date);
 $post = $recent_posts[0];
 ?>
-<div class="emergence-articles d-flex flex-sm-wrap flex-md-nowrap mt-4">
-  <div class="article-principal mb-md-4 mb-sm-4 wow slideInLeft" style="background-image: url('<?=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];?>')">
+<div class="emergence-articles d-flex flex-wrap flex-md-nowrap mt-4">
+  <div class="article-principal mb-md-4 mb-4 wow slideInLeft" style="background-image: url('<?=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];?>')">
     <div class="overlay d-flex">
       <div class="article-meta d-flex">
         <div class="article-date text-center bg-light">
@@ -33,7 +33,7 @@ $post = $recent_posts[0];
   <?php for ($i = 1; $i < count($recent_posts); $i++):
     $post = $recent_posts[$i];
     ?>
-    <div class="article mb-md-4 ml-md-4 mb-sm-4 wow slideInRight" style="background-image: url('<?=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];?>')">
+    <div class="article mb-md-4 ml-md-4 mb-4 wow slideInRight" style="background-image: url('<?=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];?>')">
       <div class="overlay d-flex">
         <div class="article-meta d-flex">
           <div class="article-date text-center bg-light">
